e00de504695d11416f4090bfdc3d7241f7b6ec61
id880447 <andru.onciul.ext@proximus.com>
2022-07-14 14:59:55 +0200

    added logo on page + instalation in README

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # FastApiTutorial
 
+## Installation
+
+create a ptyhon virtual environnement and install all the needed libraries
+
+```
+conda create -n fastApiEnv python=3.9
+conda install -n fastApiEnv --file requirements.txt
+```
  
-## used sources
+## Used ressources (bibliography)
+
+
 tutorial from https://www.fastapitutorial.com/blog/fastapi-course/
 navbar.html https://getbootstrap.com/docs/5.0/components/navbar/
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 #main.py 
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
@@ -8,9 +9,20 @@ def include_router(app):
 	app.include_router(general_pages_router)
 
 
+def configure_static(app):  #to support static files
+	"""
+		Basically, we are informing fastapi that we are going to keep all our static files in a folder named 'static' 
+		and whenever it has to search for a static file, say an image, don't search here and there. 
+		It will be inside static folder only
+	"""
+	app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
 def start_application():
 	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
+	configure_static(app)
 	return app 
 
 
